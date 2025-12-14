932a43c57a60333399a6bc15647b765ffc094af3
Nilamani Dhal <dhalnilamani398@gmail.com>
2025-12-14 19:35:30 +0530

    disable corn service

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/medimind'
 .then(() => {
   console.log('Connected to MongoDB');
   // Start cron jobs after DB connection
-  startCronJobs();
+  // startCronJobs();
 })
 .catch(err => console.error('MongoDB connection error:', err));
 
